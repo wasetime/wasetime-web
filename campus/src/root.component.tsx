58a25ff632b0e030720c9e835eab3f43b269eebc
Zhao-Huaibo <huaibozhao3@gmail.com>
2021-02-25 12:28:55 +0900

    added room searching function

--- campus/src/root.component.tsx
+++ campus/src/root.component.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./configs/i18n.ts";
 import Bus from "./bus/Bus";
+import RoomFinder from "./room/RoomFinder"
+// import 'semantic-ui-css/semantic.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export default function Root(props) {
   return (
     <section>
+      <RoomFinder />
       <Bus />
     </section>
   );
